f8d43c9e8e80ce6cb1b5cac8b5871c8480cf450e
Harish A <harish18092002@gmail.com>
2023-02-20 22:44:18 +0530

    Observables and map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'version';
+  // observables
+  constructor() {
+    const test$ = new Observable(Subscriber => {
+      console.log("Hi")
+      Subscriber.next("1")
+      Subscriber.next("2")
+      Subscriber.next("3")
+    });
+
+    test$.subscribe(a => {
+      console.log(a)
+    })
+
+    // map function
+
+    const customers = [
+      { id: 1, first_name: "harish", last_name: "a" },
+      { id: 2, first_name: "abin", last_name: "b" },
+
+      { id: 3, first_name: "madan", last_name: "c" }
+
+
+    ]
+
+    let details = customers.map(function (val) {
+      let Fullname = [val.first_name, val.last_name].join(" ")
+      let output = { ID: val.id, Fullname: Fullname }
+      return output
+    })
+
+    console.log(details)
+  }
+
+
+
+
+
 }
